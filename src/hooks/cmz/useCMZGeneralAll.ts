09e037bea3f364702fc7276c6638fab3445ade3b
Edmilbe <bm.edmilbe@gmail.com>
2026-02-04 06:09:09 +0000

    Refactor(routing): ensure consistency with trailing slash on API routes

--- src/hooks/cmz/useCMZGeneralAll.ts
+++ src/hooks/cmz/useCMZGeneralAll.ts
@@ -2,7 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import ApiClient, { apiCMZEndpoint } from "../../services/api-client";
 
 const useCMZGeneralAll = <T>(endpoint: string, queryKey: string) => {
-  const apiClient = new ApiClient<T>(`${apiCMZEndpoint}/${endpoint}`);
+  const apiClient = new ApiClient<T>(`${apiCMZEndpoint}/${endpoint}/`);
   return useQuery({
     queryFn: apiClient.getAll,
     queryKey: [queryKey],
